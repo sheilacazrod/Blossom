47adca7ec8a667bbcd0df26f07cc4801c995a446
sheilacazrod <145383241+sheilacazrod@users.noreply.github.com>
2024-03-05 10:52:23 +0000

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {StreamingPageComponent} from "./streaming-page/streaming-page.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StreamingPageComponent],
+  imports: [RouterOutlet, StreamingPageComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
