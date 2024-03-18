19afae8ab83388717eb50fadebe5df81f8c8e236
Micael <mguerrargz@gmail.com>
2024-03-18 18:37:17 +0000

    Merged sidenav into backend.

--- frontend/src/app/streaming-page/streaming-page.component.ts
+++ frontend/src/app/streaming-page/streaming-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {StreamerSidebarComponent} from "../streamer-sidebar/streamer-sidebar.component";
 import {SideBarComponent} from "../side-bar/side-bar.component";
 import {VgCoreModule} from "@videogular/ngx-videogular/core";
+import {SidenavComponent} from "../sidenav/sidenav.component";
 
 @Component({
   selector: 'app-streaming-page',
@@ -9,11 +10,13 @@ import {VgCoreModule} from "@videogular/ngx-videogular/core";
   imports: [
     StreamerSidebarComponent,
     SideBarComponent,
-    VgCoreModule
+    VgCoreModule,
+    SidenavComponent
   ],
   templateUrl: './streaming-page.component.html',
   styleUrl: './streaming-page.component.css'
 })
 export class StreamingPageComponent {
+  extended = false;
 
 }
