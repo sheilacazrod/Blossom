dec98a74a8e33a020629a6659c54648f21104f06
Andrew-022 <145354381+Andrew-022@users.noreply.github.com>
2024-04-09 11:57:42 +0100

    pop-ups register y login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,6 +6,7 @@ import {VgCoreModule} from '@videogular/ngx-videogular/core';
 import {VgControlsModule} from '@videogular/ngx-videogular/controls';
 import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
 import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
+import { MatDialogModule } from "@angular/material/dialog";
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,7 @@ import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
     VgControlsModule,
     VgOverlayPlayModule,
     VgBufferingModule,
+    MatDialogModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { SignUpComponent } from "../sign-up/sign-up.component";
+import { LoginComponent } from "../login/login.component"
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    SignUpComponent,
+    LoginComponent
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
 
+export class NavbarComponent {
+  constructor(private dialogRef: MatDialog) {
+  }
+
+  openDialog(i: number){
+    if(i==0){
+      this.dialogRef.open(LoginComponent);
+    }
+    else{
+      this.dialogRef.open(SignUpComponent)
+    }
+  }
 }
