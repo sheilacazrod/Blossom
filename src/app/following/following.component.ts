b7a58cbc0310a4c5162ca10d22fa30dad76516fc
Alejandro Pérez Viera <jamon9990@gmail.com>
2024-04-12 10:30:56 +0100

    Todo funciona aquí comit otra vez por error en imagen de perfile

--- src/app/following/following.component.ts
+++ src/app/following/following.component.ts
@@ -13,16 +13,15 @@ export class FollowingComponent implements OnInit {
   imagenDirecto3 = 'https://ih1.redbubble.net/image.3092348941.4978/st,small,845x845-pad,1000x1000,f8f8f8.jpg';
   imagenDirecto4 = 'https://ih1.redbubble.net/image.3092348941.4978/st,small,845x845-pad,1000x1000,f8f8f8.jpg';
 
+
   imagenRecomendado1 = 'https://ih1.redbubble.net/image.3092348941.4978/st,small,845x845-pad,1000x1000,f8f8f8.jpg';
   imagenRecomendado2 = 'https://ih1.redbubble.net/image.3092348941.4978/st,small,845x845-pad,1000x1000,f8f8f8.jpg';
   imagenRecomendado3 = 'https://ih1.redbubble.net/image.3092348941.4978/st,small,845x845-pad,1000x1000,f8f8f8.jpg';
   imagenRecomendado4 = 'https://ih1.redbubble.net/image.3092348941.4978/st,small,845x845-pad,1000x1000,f8f8f8.jpg';
 
   constructor() {
-    // El constructor está vacío porque las propiedades ya están inicializadas
   }
 
   ngOnInit() {
-    // No es necesario inicializar las imágenes aquí porque ya están inicializadas
   }
 }
