b9089630c767165df052be1743b292b1a6b272bc
sheilacazrod <145383241+sheilacazrod@users.noreply.github.com>
2024-04-18 22:06:10 +0100

    reactive forms

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private fb: FormBuilder) {
+  }
+  loginForm = this.fb.group({
+    loginUser: ['', [Validators.required]],
+    loginPwd: ['',[ Validators.required,]]
+  });
 
+  submit() {
+    if (this.loginForm.valid) {
+      console.log('Usuario autenticado')
+    }
+    else{
+      console.log('Error')
+    }
+  }
 }
--- frontend/src/app/sign-up/sign-up.component.ts
+++ frontend/src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-
+  constructor(private fb: FormBuilder) {
+  }
+  registerForm = this.fb.group({
+    username: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+    password: ['', [Validators.required,Validators.maxLength(16),Validators.minLength(8)]],
+    day: ['', [Validators.required, Validators.min(1), Validators.max(31)]],
+    month: ['', [Validators.required]],
+    year: ['', [Validators.required, Validators.min(1900), Validators.max(2024)]],
+    email: ['', [Validators.required, Validators.email, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]]
+  })
+  submit() {
+    if (this.registerForm.valid) {
+      console.log('Usuario registrado')
+    }
+    else {
+      console.log('Error en los datos')
+    }
+  }
 }
