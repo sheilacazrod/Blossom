ff86bb2d9331fd037e87db9db8bc4db4618c54d2
Andrew-022 <145354381+Andrew-022@users.noreply.github.com>
2024-04-22 20:00:57 +0100

    route live users

--- frontend/src/app/streaming-page/streaming-page.component.ts
+++ frontend/src/app/streaming-page/streaming-page.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {StreamerSidebarComponent} from "../streamer-sidebar/streamer-sidebar.component";
 import {SideBarComponent} from "../side-bar/side-bar.component";
 import {VgCoreModule} from "@videogular/ngx-videogular/core";
 import {SidenavComponent} from "../sidenav/sidenav.component";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-streaming-page',
@@ -16,22 +17,33 @@ import {SidenavComponent} from "../sidenav/sidenav.component";
   templateUrl: './streaming-page.component.html',
   styleUrl: './streaming-page.component.css'
 })
-export class StreamingPageComponent {
-  src1: string = "http://167.71.61.5/josito";
-  src2: string = "http://167.71.61.5/mica";
-  changeSrc(a: number) {
+export class StreamingPageComponent{
+  usuario: string = "";
+  src: string = "http://167.71.61.5/";
 
-    const playerIframe = document.getElementById('player') as HTMLIFrameElement;
-    if (a==1){
-      if (playerIframe) {
-        playerIframe.src = this.src1;
-      }
-    }
-    if (a==2){
-      if (playerIframe) {
-        playerIframe.src = this.src2;
-      }
-    }
+  constructor(private route: ActivatedRoute) { }
 
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.usuario = params['usuario'];
+      this.src = this.src + this.usuario;
+      console.log('Usuario:', this.usuario);
+    });
   }
+
+  // changeSrc(a: number) {
+  //
+  //   const playerIframe = document.getElementById('player') as HTMLIFrameElement;
+  //   if (a==1){
+  //     if (playerIframe) {
+  //       playerIframe.src = this.src1;
+  //     }
+  //   }
+  //   if (a==2){
+  //     if (playerIframe) {
+  //       playerIframe.src = this.src2;
+  //     }
+  //   }
+  //
+  // }
 }
